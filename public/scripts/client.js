bad87669887064666d9592d5e0be37ef9c4ba646
Bobby Uno <robert.y.uno@gmail.com>
2020-02-28 23:06:01 +0000

    removed commented code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,34 +8,6 @@
 $(document).ready(function () {
   //console log appears on the console (dev tools) on page.
 
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd"
-  //     },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ];
-
-  //$(".tweetsContainer").scrollIntoView();
-
   console.log("ready!");
   // --- our code goes here ---
   const createTweetElement = function (tweet) {
